fa289a204f006c6dce3e123bc61c077cdefeb01a
Gentrit Hoxha <gentrit.hoxha2001@gmail.com>
2021-05-26 13:53:27 +0200

    Manipulating with objects and array in JavaScript

--- Jonas Schmetman- JS from zero to HERO/Part 2/part2.js
+++ Jonas Schmetman- JS from zero to HERO/Part 2/part2.js
@@ -109,6 +109,103 @@ var WINNER = checkWinner(avgTeam(10,58,96), avgTeam(5,4,25));
 console.log(WINNER);
 
 
+console.log(`------------------------------------------------------`);
+
+
+const friend1 = "Peter";
+const friend2 = "Michael";
+const friend3 = "Jonas";
+
+ 
+
+ // Arrays jane objekte
+
+   const friends = ['Peter', 'Michael', 'Jonas'];
+   console.log(typeof friends);
+   console.log(friends);
+
+   const ages  = [16,18,25,58,45,28];
+   console.log(ages);
+
+   //mix arrays
+    // const mixArray = ["Peter",85.25, 18.25, new Date(), ages, friends];
+    // console.log(mixArray);
+
+
+
+
+    /**-----Coding chanllenge 2 from the JavaScript part2 -------*/
+
+
+    const calcTip = (bill) => {
+         var tip ;
+         if (bill>=0 & bill<50) {
+                 tip = 0;
+         }
+         else if (bill>= 50 && bill <= 300) {
+             tip = bill * 0.15;
+         } else if (bill>300) {
+              tip = bill * 0.20;
+         }
+         return tip;
+    }
+    
+    
+    const bills = [125,555,44];
+   // console.log("Bills: " + bills);
+
+    const tips  = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+    //console.log("Tips: " + tips);
+
+    const total  = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+   // console.log("Bills + Tips: " + total);
+
+   console.log("Bills, Tips , Total");
+     console.log(bills, tips, total);
+
+console.log('********************************-');
+
+
+
+
+     //objects 
+     const person  = {
+         firstName: "Michael",
+         lastName:  "Schimdt",
+         birthYear:   1993,
+         job: 'Teacher',
+         age: 0,
+         hasDriverLicense: false,
+         friends: ['Jonas', 'Kevin', 'Maximilian'],
+         calcAge: function () {
+            const rez = 2025 - this.birthYear;
+             return rez;
+         },
+
+         getSummary: function () {
+             const rez = `${this.firstName} is a ${this.calcAge()} years old ${this.job} and he has ${this.hasDriverLicense ? 'a': 'no'} driver license`
+         return rez;     
+    }
+
+     };
+
+
+     console.log(person);
+     console.log(person.firstName);
+     console.log(person.job);
+
+
+      //const nameKey = prompt("What you want to know about:  firstName, lastName, age , job or friends");
+      //console.log(person[nameKey]);
+
+console.log(person.calcAge());
+console.log(person.getSummary());
+
+
+
+
+
+
 
 
 
