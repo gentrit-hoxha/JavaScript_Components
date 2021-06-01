7efaae1e8b2d80b33b56251bb8f20877a07e30ad
Gentrit Hoxha <gentrit.hoxha2001@gmail.com>
2021-06-01 02:38:27 +0200

    Working with modals

---
+++ Jonas Schmetman- JS from zero to HERO/DOM/Open Modals/app.js
@@ -0,0 +1,24 @@
+
+const modal = document.querySelector('.modal');
+const overlay  = document.querySelector('.overlay');
+const openModals = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+
+function opentheModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeTheModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+for (let i = 0; i < openModals.length; i++) {
+    openModals[i].addEventListener('click' , opentheModal);
+    
+}
+
+closeModal.addEventListener('click' , closeTheModal);
+
