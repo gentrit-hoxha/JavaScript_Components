64bbc9686faee4ef796aa64eb932cf0ed0189706
Gentrit Hoxha <gentrit.hoxha2001@gmail.com>
2021-06-03 00:05:50 +0200

    Implementing Comparing BMI Project

--- Jonas Schmetman- JS from zero to HERO/DOM/Comparing BMI/bmi.js
+++ Jonas Schmetman- JS from zero to HERO/DOM/Comparing BMI/bmi.js
@@ -1,4 +1,3 @@
-  
 
   const btnCompare = document.querySelector('#compareButton');
   var markHeight = document.querySelector('#markHeight');
@@ -16,7 +15,63 @@
     var markDescription = `Mark is ${markHeight.value} cm tall and weighs ${markWeight.value} kg`;
     var johnDescription = `John is ${johnHeight.value} cm tall and weighs ${johnWeight.value} kg`;
       
-    console.log(markDescription);
-    console.log(johnDescription);
+    
+
+      displayDataInTables();
+      displayBMItable();
+
+    // console.log(markDescription);
+    // console.log(johnDescription);
+
+    markHeight.value = '';
+    markWeight.value = '';
+
+ johnHeight.value = '';
+ johnWeight.value = '';
     
   });
+
+
+  function calcBmi( height , weight ) {
+    
+      var h = height/100;
+        var rez = weight / (h*h); 
+          rez = rez.toFixed(2);
+         return  rez;
+     
+      }
+
+      function displayDataInTables() {
+        document.querySelector('.markHeightTable').textContent = markHeight.value;
+           document.querySelector('.markWeightTable').textContent = markWeight.value;
+               
+        document.querySelector('.johnHeightTable').textContent = johnHeight.value;
+                  document.querySelector('.johnWeightTable').textContent = johnWeight.value;
+      }
+
+      function displayBMItable() {
+
+        const markBMI = calcBmi(markHeight.value , markWeight.value);
+        document.querySelector('.markBmiTable').textContent = markBMI;
+
+        const johnBMI = calcBmi(johnHeight.value , johnWeight.value);
+        document.querySelector('.johnBmiTable').textContent = johnBMI;
+
+        if (markBMI > johnBMI) {
+          document.querySelector('.compareResult').textContent = "Mark's BMI is Higher than JOHN's BMI";
+        }
+        else if ( johnBMI > markBMI) {
+          document.querySelector('.compareResult').textContent = "JOHN's BMI is Higher than MARK's BMI";
+          
+        }
+
+      }
+
+
+
+
+
+
+
+
+
