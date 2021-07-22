af5e38931f46fd688c8ed036e4c202da1d360b32
Gentrit Hoxha <gentrit.hoxha2001@gmail.com>
2021-07-22 14:49:50 +0200

    Implementing the responsive design and some stuf in js of app todo files

--- todoApp/app.js
+++ todoApp/app.js
@@ -76,35 +76,33 @@ function deleteFunction(e) {
   }
 }
 
-
-
-
    function filterTheTodo(e) {
      const todos = todoList.childNodes;
 
      todos.forEach(function (todo) {
-           switch (e.target.value) {
-             case 'all':
-                 todo.style.display = 'flex'
-               break;
+          
+      switch (e.target.value) {
+        case 'all':
+          todo.style.display = 'flex'
+        break;
 
-                case 'completed':
-                  if (todo.classList.contains("completed")) {
-                    todo.style.display = "flex";
-                  } 
-                  else {
-                    todo.style.display = "none";
-                  }
-                  break;
+        case 'completed':
+          if (todo.classList.contains("completed")) {
+             todo.style.display = "flex";
+            } 
+          else {
+              todo.style.display = "none";
+            }
+        break;
                   
-                  case 'uncompleted':
-                    if (!todo.classList.contains("completed")) {
-                      todo.style.display = "flex";
-                      
-                    } else {
-                      todo.style.display = "none";
-                   }
-             
+        case 'uncompleted':
+          if(!todo.classList.contains("completed")) {
+              todo.style.display = "flex";  
+              } 
+           else {
+              todo.style.display = "none";
+             }
+         break;    
            }
      });
    }
