3bb3d8783aef229f4510d19e6b0ccd686428b60b
Gentrit Hoxha <gentrit.hoxha2001@gmail.com>
2021-09-02 21:57:57 +0200

    flags and names of all the countries with RESTful API getting the data in dynamically from in JSON files

--- Fajlla Testues/jquery-1.js
+++ Fajlla Testues/jquery-1.js
@@ -10,7 +10,7 @@ const b = $('ul');
 console.log(a);
 console.log(b);*/
 
-
+/*
 
 fetch('sample1.json')
 .then(function (response) {
@@ -41,4 +41,59 @@ fetch('sample1.json')
             
             mainContainer.appendChild(div);
           }
-    } 
+    } */
+
+
+
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+       // console.log("The capital of " + data[0].name + " is " + data[0].population);
+           displayCountries(data);
+           
+      });
+
+      function displayData(data) {
+        let country = document.querySelector('.countries');
+        for (let i = 0; i < 100; i++) {
+         //   console.log(`Country: ${data[i].name}, Language ${data[i].languages[0].name}`);      
+        
+         country.innerHTML += `Country: ${data[i].name}, Language ${data[i].languages[0].name}`+ '</br>';
+          }
+      }
+
+      function displayCountries(data) {
+        let mainContainer = document.getElementById('myData');
+              for (let i = 0; i < data.length; i++) {
+                let div = document.createElement('div');
+                    div.classList.add('user');
+    
+
+                    
+
+                    let nameEL = document.createElement('h1');
+                      nameEL.classList.add('name');
+                      nameEL.textContent = 'Country: ' + data[i].name;
+                      
+                      let surnameEL = document.createElement('h3');
+                      surnameEL.classList.add('surname');
+                      surnameEL.textContent = 'Language: ' +  data[i].languages[0].name;
+    
+                     let image = document.createElement('img');
+                     image.classList.add('size');
+                     image.src = data[i].flag + "";    
+                      
+                      div.appendChild(nameEL);
+                      div.appendChild(surnameEL);
+                      div.appendChild(image);
+
+                      
+                
+                mainContainer.appendChild(div);
+              }
+        } 
+
+
+     
